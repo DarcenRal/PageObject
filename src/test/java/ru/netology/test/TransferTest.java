fa65bdecbc59eb3517141031c41fc2e6cc71fbdc
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-09-30 19:59:58 +0300

    Add new tests

--- src/test/java/ru/netology/test/TransferTest.java
+++ src/test/java/ru/netology/test/TransferTest.java
@@ -1,5 +1,6 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.Condition;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataGenerator;
@@ -7,6 +8,8 @@ import ru.netology.page.DashboardPage;
 import ru.netology.page.LoginPage;
 
 
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -55,5 +58,40 @@ public class TransferTest {
         assertEquals(balanceAfterTransactionOnRecharged, balanceOfFirstCardAfter);
         assertEquals(balanceAfterTransaction, balanceOfSecondCardAfter);
 
+    }
+
+    @Test
+    void transferMoreThanBalanceFirstTest () {
+        int amount = 20000;
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataGenerator.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataGenerator.getVerificationCodeFor(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        val transferPage = dashboardPage.secondCardToRecharge();
+        val cardInfo = DataGenerator.getFirstCardInfo();
+        transferPage.transferOnCard(cardInfo, amount);
+        $(withText("На балансе недостаточно средств")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void transferMoreThanBalanceSecondTest() {
+        int amount = 20000;
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataGenerator.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataGenerator.getVerificationCodeFor(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        val balanceFirstCardBefore = DashboardPage.getCurrentBalanceOfFirstCard();
+        val balanceSecondCardBefore = DashboardPage.getCurrentBalanceOfSecondCard();
+        val transferPage = dashboardPage.firstCardToRecharge();
+        val cardInfo = DataGenerator.getSecondCardInfo();
+        transferPage.transferOnCard(cardInfo, amount);
+        val balanceAfterTransactionOnRecharged = DataGenerator.checkBalanceCardPlus(balanceFirstCardBefore, amount);
+        val balanceAfterTransaction = DataGenerator.checkBalanceCardMinus(balanceSecondCardBefore, amount);
+        val balanceOfFirstCardAfter = DashboardPage.getCurrentBalanceOfFirstCard();
+        val balanceOfSecondCardAfter = DashboardPage.getCurrentBalanceOfSecondCard();
+        assertEquals(balanceAfterTransactionOnRecharged, balanceOfFirstCardAfter);
+        assertEquals(balanceAfterTransaction, balanceOfSecondCardAfter);
     }
 }
